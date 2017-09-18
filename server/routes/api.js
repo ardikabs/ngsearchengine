8b91dcca5790464e289422bddf733c790c6a28a7
ardikabs <ardikabs@gmail.com>
2017-09-18 17:42:54 +0700

    testing

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,7 @@ const request = require('request');
 
 var client_id ='ca8d5e60f7874a76b8497953bf16a0a7';
 var client_secret ='f3e4b0d819444de69a4789ff29293c46';
-var options = {
+var authOptions = {
     url:'https://accounts.spotify.com/api/token',
     headers:{
         'Authorization': 'Basic '+ (new Buffer(client_id + ':' + client_secret.toString('base64'))),
@@ -16,13 +16,13 @@ var options = {
         grant_type: 'client_credentials'
       },
     json: true
-}
+};
 
-request.post(options, function(error, response, body){
+request.post(authOptions, function(error, response, body){
     if (!error && response.statusCode === 200) {
         // use the access token to access the Spotify Web API
         var token = body.access_token;
-        console.log(token);
+        console.log("Iki Token Jancok-> "+token);
     }
 });
 
@@ -32,7 +32,29 @@ router.get('/',function(req,res){
 
 
 
-router.get('/getToken',function(req,res){
+router.get('/access_token',function(req,res){
+    var client_id ='ca8d5e60f7874a76b8497953bf16a0a7';
+    var client_secret ='f3e4b0d819444de69a4789ff29293c46';
+    var authOptions = {
+        url:'https://accounts.spotify.com/api/token',
+        headers:{
+            'Authorization': 'Basic '+ (new Buffer(client_id + ':' + client_secret.toString('base64'))),
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        form: {
+            grant_type: 'client_credentials'
+          },
+        json: true
+    };
+    
+    request.post(authOptions, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            // use the access token to access the Spotify Web API
+            var token = body.access_token;
+            console.log("Iki Token Nak Njero-> "+token);
+        }
+    });
+
     res.send('Get Spotify Access Token');
 });
 
