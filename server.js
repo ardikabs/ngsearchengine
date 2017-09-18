ac0bfa2108f43da6814f499eacde84c07bbb512a
ardikabs <ardikabs@gmail.com>
2017-09-18 17:48:17 +0700

    testing

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const api = require('./server/routes/api');
+var bodyParser = require('body-parser');  
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname+"/dist"));
 
