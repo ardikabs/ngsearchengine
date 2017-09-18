d0070027dd8b3694beb033cf1c758616cc84c838
ardikabs <ardikabs@gmail.com>
2017-09-18 17:58:20 +0700

    testing

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,7 +9,7 @@ router.get('/',function(req,res){
     res.send('API Works');
 });
 
-router.get('/access_token',function(req,res){
+router.post('/access_token',function(req,res,next){
     var client_id ='ca8d5e60f7874a76b8497953bf16a0a7';
     var client_secret ='f3e4b0d819444de69a4789ff29293c46';
     var authOptions = {
@@ -23,17 +23,18 @@ router.get('/access_token',function(req,res){
           },
         json: true
     };
-    
-    request.post(authOptions, function(error, response, body){
-        console.log("Sedang REQUEST ");        
-        if (!error && response.statusCode === 200) {
-            // use the access token to access the Spotify Web API
-            var token = body.access_token;
-            console.log("Iki Token Nak Njero-> "+token);
-        }
-    });
 
-    res.send('Get Spotify Access Token');
+    request(authOptions).pipe(res);
+    console.log(res);    
+    // request.post(authOptions, function(error, response, body){
+    //     console.log("Sedang REQUEST ");        
+    //     if (!error && response.statusCode === 200) {
+    //         // use the access token to access the Spotify Web API
+    //         var token = body.access_token;
+    //         console.log("Iki Token Nak Njero-> "+token);
+    //     }
+    // }).pipe(res);
+    res.json();
 });
 
 module.exports = router;
